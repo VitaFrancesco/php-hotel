45ce262c6dd8b12c2f43f29a7e559fdc6a9e2a67
VitaFrancesco <vitafrancesco00@gmail.com>
2025-02-24 20:09:53 +0100

    Add filtering options for parking availability and minimum vote in hotel search

--- index.php
+++ index.php
@@ -40,6 +40,17 @@
 
     ];
 
+    $parking_requested = false;
+    $vote_min = 0;
+
+    if(isset($_GET['parking']) && $_GET['parking'] == "on") {
+        $parking_requested = true;
+    };
+
+    if(isset($_GET['vote']) && $_GET['vote'] > 0 & $_GET['vote'] < 6) {
+        $vote_min = $_GET['vote'];
+    };
+    
 ?>
 
 <!DOCTYPE html>
@@ -51,14 +62,36 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="d-flex flex-column" style="min-height: 100vh;" >
-    <header>
-        
+    <header class="py-2 px-4" >
+        <form action="" method="GET">
+            <input type="checkbox" name="parking" id="parking" >
+            <label for="parking">Parcheggio Disponibile</label>
+            <select name="vote" >
+                <option value="" disabled selected>Voto Minimo</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <button type="submit" >Cerca</button>
+        </form>
     </header>
     <main class="flex-grow-1" >
-        <div class="container py-5">
+        <div class="container-fluid p-5">
+            <h1 class="text-center mb-3">Hotels</h1>
             <div class="row">
-                <?php foreach ($hotels as $hotel): ?>
-                    <div class="col-md-4 mb-4">
+                <?php 
+                
+                    foreach ($hotels as $hotel) { 
+                        if($parking_requested) {
+                            if($hotel['parking'] != true) continue;
+                        }
+                        if($vote_min) {
+                            if($hotel['vote'] < $vote_min) continue;
+                        }
+                ?>
+                    <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4">
                         <div class="card h-100 shadow">
                             <div class="card-body">
                                 <h5 class="card-title"><?= $hotel['name']; ?></h5>
@@ -75,7 +108,9 @@
                             </div>
                         </div>
                     </div>
-                <?php endforeach; ?>
+                <?php 
+                        };
+                ?>
             </div>
         </div>
     </main>
