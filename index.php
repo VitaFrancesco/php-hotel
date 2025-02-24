48d79a2d77b98d4bbadbace85e93131d7aad6a95
VitaFrancesco <vitafrancesco00@gmail.com>
2025-02-24 18:47:12 +0100

    Add HTML structure and Bootstrap styling to display hotels

--- index.php
+++ index.php
@@ -40,5 +40,48 @@
 
     ];
 
-    var_dump($hotels);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="d-flex flex-column" style="min-height: 100vh;" >
+    <header>
+        
+    </header>
+    <main class="flex-grow-1" >
+        <div class="container py-5">
+            <div class="row">
+                <?php foreach ($hotels as $hotel): ?>
+                    <div class="col-md-4 mb-4">
+                        <div class="card h-100 shadow">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $hotel['name']; ?></h5>
+                                <p class="card-text"><?= $hotel['description']; ?></p>
+                                <p class="card-text">
+                                    <strong>Parcheggio:</strong> <?= $hotel['parking'] ? 'Disponibile' : 'Non disponibile'; ?>
+                                </p>
+                                <p class="card-text">
+                                    <strong>Voto:</strong> <?= $hotel['vote']; ?>
+                                </p>
+                                <p class="card-text">
+                                    <strong>Distanza dal centro:</strong> <?= $hotel['distance_to_center']; ?> km
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p class="text-center text-secondary" >@by&#58; Francesco Vita</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
